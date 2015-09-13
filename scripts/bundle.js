10b1af4ad53f69288a49a12a4748884c27e8c21b
Troy Acosta <troyacosta@sbcglobal.net>
2015-09-13 12:51:39 -0500

    push

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -5,31 +5,34 @@ var header = document.getElementById('header');
 var inputBox = document.getElementById('inputBox');
 var button = document.getElementById('button');
 var textBox = document.getElementById('textBox');
-
-textBox.style.backgroundColor = 'white';
-textBox.style.width = '30em';
-textBox.style.height = '30em';
-
-header.style.textAlign = 'center';
-header.style.color = 'white';
-
-inputBox.style.width = '25em';
-
-var render = function render() {
-	textBox.innerHTML = '';
-};
-
+var container = document.getElementById('container');
 var fullList = [];
 
-button.addEventListener('click', function (event) {
-	event.preventDefault();
-	fullList.push(inputBox.value);
-	render();
-	textBox.innerHTML = fullList.join('<br>');
-	inputBox.value = '';
-});
+container.style.width = '25em';
+container.style.margin = '3em auto';
 
-render();
+textBox.style.backgroundColor = '#fff';
+textBox.style.width = '30em';
+textBox.style.height = '30em';
+textBox.style.margin = '0 auto';
+textBox.style.padding = '1em';
+
+header.style.textAlign = 'center';
+header.style.color = '#fff';
+
+inputBox.style.width = '26em';
+
+var addItem = function addItem() {
+    event.preventDefault();
+    fullList.push(inputBox.value);
+    inputBox.value = '';
+};
+
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    textBox.innerHTML = '';
+    textBox.innerHTML = fullList.join('<br>');
+});
 
 },{}]},{},[1])
 
